5371d37965c08e755bd48d563d97baaa8f108bd3
shanmugapriyan5 <165198908+shanmugapriyan5@users.noreply.github.com>
2024-05-09 01:41:27 +0530

    Add files via upload

---
+++ testapi/urls.py
@@ -0,0 +1,9 @@
+from rest_framework.authtoken.views import obtain_auth_token
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('api/',include('api.urls')),
+    path('admin/', admin.site.urls),
+    path('gettoken/', obtain_auth_token),
+]
